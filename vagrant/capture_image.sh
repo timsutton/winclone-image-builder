dcca72684afc22c6ffcab3707e259dfc40ccdda9
Timothy Sutton <tim@synthist.net>
2017-01-10 14:43:10 -0500

    Update Ubuntu to Xenial
    - we can get rid of the 3rd-party PPA as wimtools is now included in the main distro

--- vagrant/capture_image.sh
+++ vagrant/capture_image.sh
@@ -18,32 +18,9 @@ if [ "$OUTPUT_IMAGE_FORMAT" == "winclone" ]; then
 	pigz > "${OUTPUT_PATH}"
 fi
 
-# wim output (not tested in quite a while)
+# wim output
 if [ "$OUTPUT_IMAGE_FORMAT" == "wim" ]; then
 	OUTPUT_PATH="${OUTPUT_DIR}/boot.wim"
-	echo "deb http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8.list
-	echo "deb-src http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8.list
-
-	# insert the PGP key for the Launchpad webupd8 repo
-	cat << EOF | apt-key add -
------BEGIN PGP PUBLIC KEY BLOCK-----
-Version: SKS 1.1.4
-Comment: Hostname: keyserver.ubuntu.com
-
-mI0ES1d9aAEEAN8DBnDfNfBdx79khNBGbxia9xzjOYvV1arktnVoEOo4Od2Tru3RMDTY3Ghg
-7LLoQbUGcAq3gPiIlTBLuBT/G2doNPirqP1rcK5VHJyok7CqOZBPjJM4zZEZaJ6luH6Ffizq
-X2CYE0j0dxwDwCYt1TJy/06AIB8F7NzbKrTE37wtABEBAAG0EUxhdW5jaHBhZCB3ZWJ1cGQ4
-iLYEEwECACAFAktXfWgCGwMGCwkIBwMCBBUCCAMEFgIDAQIeAQIXgAAKCRBTHucvTJ0jTKwc
-A/997aeDmJO2ugJ2OnBEI+VPHK8+ojW1PZi9sUGKTzBAEcwrSB0xt9jAvSWK8iedL5xm8WXw
-eToIPqp6FyWe7KVfKBh5v6i+kG59nGzlw6wYrcxh33XH+ko0NAPf3tT+tKHNlNyP9QrcdygS
-qpm1/uVf/iREmwpzuHfyr/CYrzh/lA==
-=EBW3
------END PGP PUBLIC KEY BLOCK-----
-EOF
-
-	# Install tools
-	apt-get update
 	apt-get install -y wimtools
-
-	wimcapture /dev/sdb1 "${OUTPUT_PATH}" --boot
+	WIMLIB_IMAGEX_USE_UTF8=1 wimcapture /dev/sdb1 "${OUTPUT_PATH}" --boot
 fi
